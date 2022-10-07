7c8e8a86f17bb25765ef66f9f00b3a6e4070d811
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2022-10-07 09:23:58 +0530

    Add project files.

---
+++ StackOverflowProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StackOverflowProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        IQuestionsService qs;
+        ICategoriesService cs;
+
+        public HomeController(IQuestionsService qs, ICategoriesService cs)
+        {
+            this.qs = qs;
+            this.cs = cs;
+        }
+        public ActionResult Index()
+        {
+            List<QuestionViewModel> questions = this.qs.GetQuestions().Take(10).ToList();
+            return View(questions);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            List<CategoryViewModel> categories = this.cs.GetCategories();
+            return View(categories);
+        }
+        [Route("allquestions")]
+        public ActionResult Questions()
+        {
+            List<QuestionViewModel> questions = this.qs.GetQuestions();
+            return View(questions);
+        }
+        public ActionResult Search(string str)
+        {
+            List<QuestionViewModel> questions = this.qs.GetQuestions().Where(temp => temp.QuestionName.ToLower().Contains(str.ToLower()) || temp.Category.CategoryName.ToLower().Contains(str.ToLower())).ToList();
+            ViewBag.str = str;
+            return View(questions);
+        }
+    }
+}
+
+
